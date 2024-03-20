5813dc64de74e5936a16dc41cc0434bf1e91c724
german-rs <18427568+german-rs@users.noreply.github.com>
2024-03-20 16:29:12 -0300

    add new file

---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+<button>Hola mundo</button>
+)
+
+//https://youtu.be/7iobxzd_2wY?si=cuTjLWf3eNJlMlQ_&t=3094
